d01cd925b73e0f38219d20ad1b77602bab7c2fac
iqbal707 <iqbal70K@gmail.com>
2026-08-09 13:28:13 +0600

    ready for vercel deployment

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 import { getAuth, getIdToken } from 'firebase/auth'; 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 })
 
 const useAxiosSecure = () => {
